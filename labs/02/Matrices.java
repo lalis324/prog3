89f9c0ccb2ec36a50f6333c993c3cacf3d7a843e
lalis324 <141380234+lalis324@users.noreply.github.com>
2023-09-04 20:16:43 -0500

    Add files via upload

---
+++ labs/02/Matrices.java
@@ -0,0 +1,32 @@
+
+package matrices;
+
+import java.util.Scanner;
+
+
+public class Matrices {
+
+
+    public static void main(String[] args) {
+        
+    }
+        public static void Suma() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Ingrese numero de filas: ");
+        int f = input.nextInt();
+        
+        System.out.print("Ingrese numero de columnas: ");
+        int c = input.nextInt();
+
+        int[][] matriz1 = new int[f][c];
+        int[][] matriz2 = new int[f][c];
+        int[][] sum = new int[f][c];
+
+        System.out.println("Ingrese los numeros de la primera matriz:");
+        
+
+        System.out.println("Ingrese los numeros de segunda matriz:");
+      
+    
+}}
