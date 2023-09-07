7f402a89e28811948eafa52daa8cda4331a03b78
lalis324 <141380234+lalis324@users.noreply.github.com>
2023-09-06 23:21:28 -0500

    Add files via upload

--- labs/02/Matriz.java
+++ labs/02/Matriz.java
@@ -11,11 +11,17 @@ public class Matriz {
         
                  Scanner input = new Scanner(System.in);
                  System.out.println("1. Suma de matrices:");
+                 System.out.println("2. Producto de matrices:");
+
           int op = input.nextInt();
 
         switch (op) {
             case 1:
                 Suma(input);
+                break;
+                
+            case 2:
+                Producto(input);
                 break;
          }
     }
@@ -46,8 +52,32 @@ public class Matriz {
                 sum[i][j] = matriz1[i][j] + matriz2[i][j];
             }
         }
+        
+    public static void Producto(Scanner input) {
+              
+        System.out.print("Ingrese el número de filas : ");
+        int f1 = input.nextInt();
+        System.out.print("Ingrese el número de columnas : ");
+        int c1 = input.nextInt();
+       
+        int[][] m1 = new int[f1][c1];
+        int[][] m2 = new int[f1][c1];
+        int[][] resultado = new int[f1][c1];
+
+        System.out.println("Ingrese la primera matriz:");
+        for (int i = 0; i < f1; i++) {
+            for (int j = 0; j < c1; j++);
+        }
+
+        System.out.println("Ingrese la segunda matriz:");
+        for (int i = 0; i < c1; i++) {
+            for (int j = 0; j < c1; j++);
+        }
+
+            }
+
     }
-}
+
 
 
 
