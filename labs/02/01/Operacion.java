04f46167659420818610d1817e2979f38d58d27a
lalis324 <141380234+lalis324@users.noreply.github.com>
2023-09-07 14:11:35 -0500

    Add files via upload

--- labs/02/01/Operacion.java
+++ labs/02/01/Operacion.java
@@ -2,22 +2,30 @@
 package matriz;
 
 public class Operacion {
-public static double[][] suma(double [f][c]matA, double [f][c] matB) {
-    double[i][j] matR; 
-     
+public static double[][] suma(double [][]matA, double [][] matB) {
+    
+    int f = matA.length;
+    int c = matA[0].length;
+    
+    double[][] matR = new double[f][c];
+    
+    int f1=matB.length;
+    int c1=matB[0].length;
+    
+   
         for (int i = 0; i < f; i++) {
-            for (int j = 0; j < c; j++); 
+            for (int j = 0; j < c; j++){}; 
                 }
 
   
-        for (int i = 0; i < f; i++) {
-            for (int j = 0; j < c; j++);
+        for (int i = 0; i < f1; i++) {
+            for (int j = 0; j < c1; j++){};
                 }
 
         for (int i = 0; i < f; i++) {
             for (int j = 0; j < c; j++) {
                 matR[i][j] = matA[i][j] + matB[i][j];
-            }
-}
-return matR;
-}
+            }}
+
+     return matR;
+}}
