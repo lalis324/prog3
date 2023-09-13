ba2082666c69ca41c4dc6cf1acd3b2596570aaa2
lalis324 <141380234+lalis324@users.noreply.github.com>
2023-09-12 22:31:57 -0500

    Add files via upload

---
+++ labs/02/02/metodos.java
@@ -0,0 +1,24 @@
+
+public class metodos {
+    
+    public static void main(String[] args) {
+        int[] array = { 100, 500, 1000, 5000, 10000 };
+        
+    }
+    public static void burbuja (double[] arr){
+            
+        }
+        
+    public static void inserción  (double[] arr){
+            
+        }
+    public static void  selección  (double[] arr){
+            
+        }     
+             
+    public static void  mergesort  (double[] arr){
+            
+        }           
+                        
+
+}
