f873cfff2e8b2f3f271f74d061581b414bb054d1
lalis324 <141380234+lalis324@users.noreply.github.com>
2023-09-26 23:04:53 -0500

    Add files via upload

---
+++ labs/03/Lab0302/Ingresar.java
@@ -0,0 +1,15 @@
+
+import java.util.Date;
+
+
+public class Ingresar {
+    private String placa;
+    private String tipo;
+    private Date fechaHoraIngreso;
+
+    public Ingresar(String placa, String tipo, Date fechaHoraIngreso) {
+        this.placa = placa;
+        this.tipo = tipo;
+        this.fechaHoraIngreso = fechaHoraIngreso;
+    }
+}
